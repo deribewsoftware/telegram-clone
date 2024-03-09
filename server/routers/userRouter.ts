bc60442926111bc5f75789403276766fd536d3f3
deribewsoftware <deribewsoftware@gmail.com>
2024-03-09 09:21:33 +0300

    updated

--- server/routers/userRouter.ts
+++ server/routers/userRouter.ts
@@ -1,15 +1,21 @@
 import {Request,Response,Router} from "express"
 import { User } from "../models/userModel";
+import "dotenv/config"
+import jwt from "jsonwebtoken"
 const userRouter=Router();
 userRouter.post('/auth/',async(req:Request,res:Response) => {
   const user=new User(req.body)
-  try{
+  try {
     await user.save();
-    res.send(user);
-
-  }
-  catch(err){
+    const accessToken = jwt.sign(
+      user.toObject(),
+      process.env.ACCESS_TOKEN_SECRET!
+    );
+    res.setHeader("Set-Cookie", `user=${accessToken}; Path=/`);
+    res.send("user created");
+  } catch (err) {
     console.log(err);
+    res.status(500).send(err);
   }
 });
 
